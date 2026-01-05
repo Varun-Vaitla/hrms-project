135204b69efdfa28b92a84d61ef5b770fd180665
Naveen63622 <naveennithin69@gmial.com>
2026-01-05 15:28:02 +0530

    Fixed Tailwind CSS setup and removed Vite default styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 const App = () => {
   return (
-    <div>
-      <h1>HRMS Frontend Started</h1>
+    <div className="h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        HRMS Tailwind Working 🚀
+      </h1>
     </div>
   );
 };
 
 export default App;
+
